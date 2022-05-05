518e0b35848855437e7d8284e06faff1b5e78670
Ben Cyna <benjamincyna@gmail.com>
2022-05-05 15:34:52 +0930

    added tail recursive function

--- main.cpp
+++ main.cpp
@@ -3,22 +3,40 @@
 using namespace std;
 
 int sumOfCube(int n);
+int tailRecursiveHelperSumOfCube(int n, int total);
 
 
 int main() {
     
-cout << sumOfCube(4);
+    cout << sumOfCube(4);
 
 
     return 0; 
 }
 
-int sumOfCube(int n) {
+// int sumOfCube(int n) {
+//     if (n <= 1) {
+//         return 1;
+//     }
+
+//     int cubed = n * n * n;
+
+//     return cubed + sumOfCube(n-1);
+// }
+
+// Tail recursive below
+
+int tailRecursiveHelperSumOfCube(int n, int total) {
     if (n <= 1) {
-        return 1;
+        return total+1;
     }
 
     int cubed = n * n * n;
-
-    return cubed + sumOfCube(n-1);
+    int newTotal = cubed + total;
+    
+    return tailRecursiveHelperSumOfCube(n-1, newTotal);
 }
+
+ int sumOfCube(int n) {
+    return tailRecursiveHelperSumOfCube(n, 0);
+ }
